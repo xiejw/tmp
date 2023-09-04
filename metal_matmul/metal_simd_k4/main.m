7c7bb9b078d96b16af39f41ad36384ee2d179472
Jianwei Xie <xiejw0217+db@gmail.com>
2023-09-03 22:00:25 -0700

    [metal_simdgroup] tested simdgroup.

---
+++ metal_matmul/metal_simd_k4/main.m
@@ -0,0 +1,35 @@
+// vim: shiftwidth=8 tabstop=8 softtabstop=8
+
+#import <Foundation/Foundation.h>
+#import <Metal/Metal.h>
+
+#import <assert.h>
+
+#import "matmul_op.h"
+
+// be consistant with other tests.
+// TODO
+// #define N_DIM (4096 * 2)
+#define N_DIM (8)
+
+int main(int argc, const char * argv[]) {
+        @autoreleasepool {
+
+                id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+                struct matmul_op * op = matmulOpNew(
+                                device,
+                                /*m=*/N_DIM,
+                                /*n=*/N_DIM,
+                                /*k=*/N_DIM);
+                assert(op != NULL);
+                NSLog(@"device prepared and function compiled");
+
+                matmulOpPrepareData(op);
+                matmulOpRun(op);
+
+                NSLog(@"execution finished");
+exit:
+                matmulOpFree(op);
+        }
+        return 0;
+}
