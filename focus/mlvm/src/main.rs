d25c4ead7e7b2133f02a06b3063cfa2e8294d982
xiejw <xiejw.io+dt@gmail.com>
2024-04-01 14:50:40 -0700

    [mlvm] started mlvm project.

--- focus/mlvm/src/main.rs
+++ focus/mlvm/src/main.rs
@@ -2,30 +2,37 @@ use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
-//#[grammar = "csv.pest"]
 #[grammar_inline = r#"
-number = { ASCII_DIGIT+ }                // one or more decimal digits
-enclosed = { "(.." ~ number ~ "..)" }    // for instance, "(..6472..)"
-sum = { number ~ " + " ~ number }        // for instance, "1362 + 12"
+WHITESPACE = _{ " " | "\t" | "\n" }
+
+alpha = _{ 'a'..'z' | 'A'..'Z' }
+digit = _{ '0'..'9' }
+ident = @{ alpha ~ (alpha | digit)* }
+
+expr = { ident }
+
+line = { "let"? ~ ident ~ "="  ~ expr ~ ";" }
+program_unit = { line+ }
+program = _{ SOI ~ program_unit ~ EOI }
 "#]
-pub struct CSVParser;
+pub struct ProgramParser;
+
+static SAMPLE: &str = "let a = b; \n c = a; c = b;\n ";
 
 fn main() {
-    let pairs = CSVParser::parse(Rule::sum, "1773 + 1362")
+    let pairs = ProgramParser::parse(Rule::program, SAMPLE)
         .unwrap()
         .next()
         .unwrap()
         .into_inner();
 
-    let numbers = pairs
-        .clone()
-        .map(|pair| str::parse(pair.as_str()).unwrap())
-        .collect::<Vec<i32>>();
-    assert_eq!(vec![1773, 1362], numbers);
+    let lines = pairs
+        .map(|pair| (pair.as_str(), pair.as_rule()))
+        .collect::<Vec<(&str, Rule)>>();
 
-    for (found, expected) in pairs.zip(vec!["1773", "1362"]) {
-        assert_eq!(Rule::number, found.as_rule());
-        println!("str => {}", found.as_str());
-        assert_eq!(expected, found.as_str());
+    println!("lines total: {}", lines.len());
+
+    for l in &lines {
+        println!("program line: {:?}, {}", l.1, l.0);
     }
 }
