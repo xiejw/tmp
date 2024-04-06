94ef6e3d2a8308d4c2d8671d94b9f10f859b5157
xiejw.io+minim2 <xiejw.io+minim2@gmail.com>
2024-04-05 19:21:19 -0700

    [mlvm] added fake codegen framework.

--- focus/mlvm/src/codegen.rs
+++ focus/mlvm/src/codegen.rs
@@ -3,6 +3,9 @@ use std::error::Error;
 use crate::ast::*;
 
 pub fn compile(ast: &Tree) -> Result<(), Box<dyn Error>> {
+    // check type
+    // check fn signature
+    // check var def
     let mut codegen = CodeGen {};
     for s in ast.statements.iter() {
         codegen.visit_statement(s);
@@ -28,21 +31,44 @@ impl Visitor<()> for CodeGen {
     }
 
     fn visit_let_statement(&mut self, s: &LetStatement) {
-        println!("generate expr");
         self.visit_expr(&s.expr);
-        println!("and assign to {}", s.ident.name);
+        self.generate_code(format!("assign to: {}", s.ident.name).as_str());
     }
 
     fn visit_assign_statement(&mut self, s: &AssignStatement) {
-        println!("generate expr");
         self.visit_expr(&s.expr);
-        println!("and assign to {}", s.ident.name);
+        self.generate_code(format!("assign to {}", s.ident.name).as_str());
     }
 
     fn visit_expr(&mut self, e: &Expr) {
         match e {
-            Expr::Ident(ref ident) => println!("lookup the ident {}", ident.name),
-            _ => println!("handle other kind of expr"),
+            Expr::FnCall(ref fn_call) => {
+                for arg in fn_call.args.iter().rev() {
+                    self.visit_expr(arg.as_ref());
+                }
+                self.generate_code(format!("lookup the fn: {}", fn_call.name).as_str());
+                self.generate_code("fn call");
+            }
+            Expr::PathLookup(ref lookup) => {
+                self.generate_code("use path scope: global");
+                let path = lookup
+                    .paths
+                    .iter()
+                    .map(|x| x.name.clone())
+                    .collect::<Vec<_>>()
+                    .join("/");
+                self.generate_code(format!("lookup the path: {}", path).as_str());
+            }
+            Expr::Ident(ref ident) => {
+                self.generate_code(format!("lookup the ident: {}", ident.name).as_str())
+            }
+            Expr::Expr(ref expr) => self.visit_expr(expr.as_ref()),
         }
+    }
+}
+
+impl CodeGen {
+    fn generate_code(&self, fake_code: &str) {
+        println!("-> {}", fake_code)
     }
 }
