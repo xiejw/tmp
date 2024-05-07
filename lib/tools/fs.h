8b88bf36a6c7e49d28b1b75a1b7050d39005b121
Jianwei Xie <xiejw0217+db@gmail.com>
2024-05-07 15:26:43 -0700

    [tools] added birthtime.

---
+++ lib/tools/fs.h
@@ -0,0 +1,64 @@
+// vim: ft=cpp
+#pragma once
+
+namespace eve::fs {
+
+struct FsDir {
+  private:
+    std::string DirName;
+    DIR        *Dir = nullptr;
+
+  public:
+    FsDir( std::string DirName, DIR *Dir ) : DirName( DirName ), Dir( Dir )
+    {
+        DEBUG( "opening dir: %s\n", this->DirName.c_str( ) );
+    };
+
+    ~FsDir( )
+    {
+        if ( Dir != nullptr ) {
+            closedir( Dir );
+            DEBUG( "closing dir: %s\n", this->DirName.c_str( ) );
+        }
+    }
+
+    // The name lifetime is limited to the invocation.
+    void Run( const std::function<void( const char *RegFileName )> Fn )
+    {
+        struct dirent *dp;
+        for ( ;; ) {
+            errno = 0;
+            dp    = readdir( Dir );
+            if ( dp == NULL ) break;
+
+            if ( strcmp( dp->d_name, "." ) == 0 ||
+                 strcmp( dp->d_name, ".." ) == 0 ) {
+                continue;
+            }
+
+            if ( dp->d_name[0] == '.' ) {
+                continue;
+            }
+
+            if ( dp->d_type == DT_REG ) {
+                Fn( dp->d_name );
+            }
+        }
+
+        if ( errno != 0 ) {
+            ALERT( "error for readir" );
+        }
+    }
+};
+
+// Return nullptr if error.
+FsDir *
+OpenDir( const char *dir )
+{
+    DIR *OsDir = opendir( dir );
+    if ( dir == nullptr ) {
+        return nullptr;
+    }
+    return new FsDir( /*name=*/dir, OsDir );
+}
+}  // namespace eve::fs
--- lib/tools/main.cc
+++ lib/tools/main.cc
@@ -1,52 +1,49 @@
 #include <cstdio>
+#include <functional>
 #include <memory>
 #include <string>
+#include <tuple>
+#include <vector>
 
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
 
-#define DEBUG( fmt, ... ) printf( fmt __VA_OPT__(, ) __VA_ARGS__ )
+#define INFO( fmt, ... )  printf( "INFO : " fmt __VA_OPT__(, ) __VA_ARGS__ )
+#define DEBUG( fmt, ... ) printf( "DEBUG: " fmt __VA_OPT__(, ) __VA_ARGS__ )
+#define ALERT( fmt, ... ) printf( "ALERT: " fmt __VA_OPT__(, ) __VA_ARGS__ )
 
-namespace eve::fs {
-struct FsDir {
-  private:
-    std::string DirName;
-    DIR        *Dir = nullptr;
-
-  public:
-    FsDir( std::string DirName, DIR *Dir ) : DirName( DirName ), Dir( Dir )
-    {
-        DEBUG( "opening dir: %s\n", this->DirName.c_str( ) );
-    };
-
-    ~FsDir( )
-    {
-        if ( Dir != nullptr ) {
-            closedir( Dir );
-            DEBUG( "closing dir: %s\n", this->DirName.c_str( ) );
-        }
-    }
-};
-
-FsDir *
-OpenDir( const char *dir )
-{
-    DIR *OsDir = opendir( dir );
-    if ( dir == nullptr ) {
-        return nullptr;
-    }
-    return new FsDir( /*name=*/dir, OsDir );
-}
-}  // namespace eve::fs
+#include "fs.h"
 
 int
 main( )
 {
-    printf( "Hello, opendir(\".\") \n" );
     auto dir = std::unique_ptr<eve::fs::FsDir>{ eve::fs::OpenDir( "." ) };
     if ( dir == nullptr ) {
-        printf( "failed to open dir\n" );
+        ALERT( "failed to open dir\n" );
         exit( 1 );
+    }
+
+    std::vector<std::pair<std::string, time_t>> FileNames{ };
+    int                                         count = 0;
+    dir->Run( [&]( const char *RegFileName ) {
+        struct stat sb;
+        if ( stat( RegFileName, &sb ) == -1 ) {
+            ALERT( "failed\n" );
+        } else {
+            time_t BirthTime = sb.st_birthtime;
+            INFO( "birthtime for %s: %s", RegFileName, ctime( &BirthTime ) );
+            FileNames.push_back( { RegFileName, BirthTime } );
+        }
+        count++;
+    } );
+    INFO( "total %d files\n", count );
+
+    std::sort( FileNames.begin( ), FileNames.end( ),
+               []( auto a, auto b ) { return a.second < b.second; } );
+    for ( auto &f : FileNames ) {
+        INFO( "birthtime: %ld: %s\n", f.second, f.first.c_str( ) );
     }
 
     return 0;
