ee5c07acf769eb5b906481de397a645a7a7c6c80
Jianwei Xie <xiejw0217+db@gmail.com>
2024-05-07 15:53:04 -0700

    [tools] added symbol link.

--- lib/tools/main.cc
+++ lib/tools/main.cc
@@ -7,14 +7,30 @@
 
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <unistd.h>
 
 #define INFO( fmt, ... )  printf( "INFO : " fmt __VA_OPT__(, ) __VA_ARGS__ )
 #define DEBUG( fmt, ... ) printf( "DEBUG: " fmt __VA_OPT__(, ) __VA_ARGS__ )
 #define ALERT( fmt, ... ) printf( "ALERT: " fmt __VA_OPT__(, ) __VA_ARGS__ )
 
 #include "fs.h"
+
+std::string
+Rename( std::string &Src, int Index )
+{
+    char        Buf[1000];
+    const char *FileName = Src.c_str( );
+    auto        ext      = strrchr( FileName, '.' );
+    if ( !ext ) {
+        snprintf( Buf, 1000 - 1, "%010d", Index );
+    } else {
+        snprintf( Buf, 1000 - 1, "%010d%s", Index, ext );
+    }
+    return Buf;
+}
 
 int
 main( )
@@ -42,8 +58,17 @@ main( )
 
     std::sort( FileNames.begin( ), FileNames.end( ),
                []( auto a, auto b ) { return a.second < b.second; } );
+
+    int Index = 0;
     for ( auto &f : FileNames ) {
-        INFO( "birthtime: %ld: %s\n", f.second, f.first.c_str( ) );
+        std::string &Src = f.first;
+        std::string  Dst = Rename( f.first, Index++ );
+        INFO( "birthtime: %ld: %s\n", f.second, Src.c_str( ) );
+        INFO( "Rename to %s\n", Dst.c_str( ) );
+        if ( -1 == symlink( Src.c_str( ), Dst.c_str( ) ) ) {
+            ALERT( "unexpected error\n" );
+            exit( 1 );
+        }
     }
 
     return 0;
