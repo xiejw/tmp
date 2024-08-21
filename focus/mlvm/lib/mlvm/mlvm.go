3e77fc0563a2c2606dd0c604419b201a16f5042d
xiejw <xiejw.io+dt@gmail.com>
2024-08-21 15:16:29 -0700

    [mlvm] Added vm and program interface.

--- focus/mlvm/lib/mlvm/mlvm.go
+++ focus/mlvm/lib/mlvm/mlvm.go
@@ -1,11 +1,19 @@
 package mlvm
 
+import "io"
+
 type VM interface {
-	PrepareProgram() error
+	io.Closer
+
+	PrepareProgram(string) (Program, error)
+	Unmarshal([]byte) (Program, error)
 }
 
-type Shape struct {
-	Dims []int
+type Program interface {
+	io.Closer
+
+	Run([]TensorPtr) ([]TensorPtr, error)
+	Marshal() ([]byte, error)
 }
 
 type DataType int
@@ -15,11 +23,45 @@ const (
 	I64
 )
 
+type Shape struct {
+	dtype DataType
+	dims  []int
+}
+
+type TensorPtr interface {
+	Name() string
+	Shape() Shape // Shape
+	Ptr() any     // Raw pointer
+}
+
 // -----------------------------------------------------------------------------
 // Shape methods
 //
 
-func (s Shape) Rank() int { return len(s.Dims) }
+func NewShape(dtype DataType, dims ...int) Shape {
+	return Shape{dtype: dtype, dims: dims}
+}
+
+func (s Shape) DataType() DataType { return s.dtype }
+func (s Shape) Rank() int          { return len(s.dims) }
+func (s Shape) Dims() []int        { return s.dims }
+func (s Shape) NumElems() int {
+	c := 1
+	for _, d := range s.dims {
+		c *= d
+	}
+	return c
+}
+func (s Shape) ByteSizes() int {
+	switch s.dtype {
+	case F32:
+		return 4 * s.NumElems()
+	case I64:
+		return 8 * s.NumElems()
+	default:
+		panic("unsupported")
+	}
+}
 
 type simpleVM struct {
 }
--- focus/mlvm/lib/mlvm/mlvm_test.go
+++ focus/mlvm/lib/mlvm/mlvm_test.go
@@ -2,6 +2,8 @@ package mlvm
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // -----------------------------------------------------------------------------
@@ -9,13 +11,10 @@ import (
 //
 
 func TestShapeRank(t *testing.T) {
-
-	s := Shape{
-		Dims: []int{1, 2, 4},
-	}
-
-	if s.Rank() != 3 {
-		t.Errorf("rank mismatch.")
-	}
-
+	s := NewShape(F32, 1, 2, 4)
+	assert.Equal(t, F32, s.DataType(), "dtype mismatch.")
+	assert.Equal(t, 3, s.Rank(), "rank mismatch.")
+	assert.Equal(t, []int{1, 2, 4}, s.Dims(), "dims mismatch.")
+	assert.Equal(t, 8, s.NumElems(), "elem count mismatch.")
+	assert.Equal(t, 32, s.ByteSizes(), "elem count mismatch.")
 }
