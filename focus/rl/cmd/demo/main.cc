dac40cfe1bc1c9c50252a913fded1bea6cf82830
xiejw <xiejw.io+dt@gmail.com>
2024-09-19 08:37:04 -0700

    [rl] added first version of rl.

---
+++ focus/rl/cmd/demo/main.cc
@@ -0,0 +1,12 @@
+#include <print>
+
+int
+main( )
+{
+    std::println( "Hello" );
+    std::println( "sx.." );
+    std::println( "...." );
+    std::println( "..x." );
+    std::println( "..xg" );
+    return 0;
+}
