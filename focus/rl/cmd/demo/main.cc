d53871c76026201e0ed33087c4cfc58e931ed4d6
Jianwei Xie <xiejw0217+db@gmail.com>
2024-09-19 21:13:07 -0700

    [rl] Changed to vector with string_view.

--- focus/rl/cmd/demo/main.cc
+++ focus/rl/cmd/demo/main.cc
@@ -1,12 +1,30 @@
 #include <print>
+#include <span>
+#include <string_view>
+#include <vector>
+
+struct Env {
+    std::span<std::string_view> maze;
+
+    auto render( ) -> void
+    {
+        for ( auto &row : maze ) {
+            std::println( "{}", row );
+        }
+    }
+};
 
 int
 main( )
 {
-    std::println( "Hello" );
-    std::println( "sx.." );
-    std::println( "...." );
-    std::println( "..x." );
-    std::println( "..xg" );
+    std::vector<std::string_view> maze{
+        "sx..",
+        "....",
+        "..x.",
+        "..xg",
+    };
+
+    Env Env{ maze };
+    Env.render( );
     return 0;
 }
