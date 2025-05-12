bbb1124206cbda946ec764f8ae398ce67cafbd9f
Jianwei Xie <xiejw0217+db@gmail.com>
2025-05-12 09:46:46 -0700

    [blas] Added a comment for mac version.

--- 2025/2025-blas/00_mac_blas.c
+++ 2025/2025-blas/00_mac_blas.c
@@ -8,11 +8,13 @@ main( void )
     float a[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
     // 2x3
     float b[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
+    // 2x2
     float c[4];
 
+    // c = matmul(a, trans(b));
     cblas_sgemm( CblasRowMajor, CblasNoTrans, CblasTrans, 2, 2, 3, 1.0,
                  /*A=*/a, 3,
-                 /*B=*/b, 3, 0.0, /*C=*/c, 2 );
+                 /*B=*/b, 3, 0.0, /*C=*/c, /*leading_dim_c=*/2 );
 
     printf( "c %f\n", c[0] );
     printf( "c %f\n", c[1] );
