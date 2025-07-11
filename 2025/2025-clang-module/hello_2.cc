80b597ad7e898bc2fc80d1f54a71c987ca85f2b5
xiejw.io+m4max@gmail.com <xiejw.io+m4max@gmail.com>
2025-07-11 14:00:04 -0700

    [clang-module] added module support.

---
+++ 2025/2025-clang-module/hello_2.cc
@@ -0,0 +1,10 @@
+module;
+
+#include <print>
+
+module hello;
+
+namespace hello {
+    void Printer::Print( ) { std::print( "Hi from the module hello\n"); }
+};
+
---
+++ 2025/2025-clang-module/main.cc
@@ -0,0 +1,10 @@
+import mod;
+import hello;
+
+auto main() -> int {
+  demo::Printer printer;
+  printer.Print();
+  hello::Printer printer2;
+  printer2.Print();
+  return 0;
+}
