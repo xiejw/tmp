314aba9a6fed7b087475e1678c49e823793c9b52
xiejw.io+m4max@gmail.com <xiejw.io+m4max@gmail.com>
2025-09-08 21:37:33 -0700

    [map] added new code.

--- 2025/2025-map-mh/main.c
+++ 2025/2025-map-mh/main.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
 
-int main() {
-        return 0;
+typedef int map_handle_t;
+
+struct int_t {
+    int          id;
+    char        *data;
+    map_handle_t hh;
+};
+
+struct str_t {
+    char        *str;
+    char        *data;
+    map_handle_t hh;
+};
+
+typedef struct {
+} map_s;
+
+void
+map_init( map_s *m )
+{
+}
+
+void
+map_deinit( map_s *m )
+{
+}
+
+int
+main( )
+{
+    map_s m;
+
+    map_init( &m );
+
+    // map_add(
+    // map_find(
+    // map_del(
+    // map_iter(
+    // map_replace(
+
+    map_deinit( &m );
+
+    return 0;
 }
