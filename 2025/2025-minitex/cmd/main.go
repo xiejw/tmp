98c88045032ed6e387a0a1a831b0b52fe661233b
xiejw <xiejw.io+xieqi@gmail.com>
2025-10-04 07:14:54 -0700

    [minitex] started parser.

--- 2025/2025-minitex/cmd/main.go
+++ 2025/2025-minitex/cmd/main.go
@@ -7,12 +7,49 @@ import (
 )
 
 // LoadFileToString reads the contents of a file and returns it as a string.
-func LoadFileToString(path string) (string, error) {
+func LoadFileToString(path string) ([]byte, error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(data), nil
+	return data, nil
+}
+
+func EmitToken(in []byte) error {
+	p := 0
+	for p < len(in) {
+		switch {
+		case in[p] == '\\':
+			p = ReadCmd(in, p+1)
+		default:
+			panic("unimpl")
+		}
+	}
+
+	return nil
+}
+
+func ReadCmd(in []byte, p int) int {
+	start := p
+	for {
+		if p > len(in) {
+			panic("out of range")
+		}
+
+		c := in[p]
+
+		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
+			p++
+			continue
+		}
+
+		if start == p {
+			panic("cannot find cmd")
+		}
+
+		fmt.Printf("token: `%v`\n", string(in[start:p]))
+		return p
+	}
 }
 
 func main() {
@@ -21,5 +58,5 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(content)
+	EmitToken(content)
 }
